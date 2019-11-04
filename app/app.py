ece35a0829d9afde4e545976376ee48ae1d5e0fd
Daan Soeten <d.soeten@gmail.com>
2019-11-04 11:05:06 +0100

    Added Telegram API bot

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from .models.spoiler import Spoiler
 from .controllers.cache import Cache
 from .views.prettyCard import PrettyCard
 from termcolor import colored
+import telegram
+import os
 
 
 class App:
@@ -12,14 +14,13 @@ class App:
     # Cache spoiler data
     cache = Cache(spoiler)
 
-    def __init__(self):
-        card = PrettyCard(self.spoiler.get_first_set().get_first_card())
-        card.print_output()
+    # Telegram Bot
+    bot = telegram.Bot(token=os.getenv('API_TOKEN'))
 
+    def __init__(self):
         print(colored('{} new cards found'.format(len(self.spoiler.get_new_cards())), 'yellow'))
 
-        for card in self.spoiler.get_new_cards():
-            print(colored(card.get_name(), 'red'))
-
     def start(self):
-        pass
+        print(self.bot.get_me())
+
+
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 # coding=utf-8
-
 from app.app import App
+from dotenv import load_dotenv
 
-app = App()
+if __name__ == '__main__':
+    # Load .env variables
+    load_dotenv()
+
+    # Start app
+    app = App()
+    app.start()
+
+    exit(0)
